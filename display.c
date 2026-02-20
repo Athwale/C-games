41af107e35af0f2a8383f842d0a3191efe5aebed
Ondrej Mejzlik <omejzlik@redhat.com>
2026-02-20 21:15:07 +0100

    declare functions up top

--- display.c
+++ display.c
@@ -29,6 +29,17 @@ typedef struct element {
 // todo document all functions.
 // todo some stuff can be static.
 
+static void set_current_color(WINDOW *window, short color);
+void draw_game_screen(int x_length, int y_length, const ELEMENT area[x_length][y_length], const char score[]);
+void end_game_screen();
+int draw_menu(int count, char **items, int selected);
+void draw_end_screen(int score);
+short add_color(short foreground, short background);
+void set_border_color(short color_pair_number);
+void set_score_color(short color_pair_number);
+void start();
+void end();
+
 static void set_current_color(WINDOW *window, const short color) {
     if (has_colors()) {
         wcolor_set(window, color, NULL);
